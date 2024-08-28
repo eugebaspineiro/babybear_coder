872f67452c0b2489d6a041b8895a76a1189e743c
eugebaspineiro <euge.baspineiro@gmail.com>
2024-08-28 15:30:20 -0300

    base del proyecto

---
+++ js/carrito.js
@@ -0,0 +1,110 @@
+const contenedorCarrito = document.querySelector("#pag-carrito");
+
+let total= 0;
+
+let valordescuento= 0;
+
+// Codigos de descuento
+
+const descuentos = {
+    "DESC10" : 0.10,
+    "DESC20" : 0.20,
+};
+
+let carritoLS = JSON.parse(localStorage.getItem("carrito"));
+
+carritoLS.forEach(producto => {
+    let div = document.createElement("div");
+    div.classList.add("productoFlex");
+    div.innerHTML= `
+            <div class="listaProdFlex">
+            <img src= "${producto.imgcar}" class="productoTamano">                  
+            <p class="subtituloTypo">${producto.titulo}</p>
+            </div>                        
+            <p class="subtituloTypo">${producto.cantidad}</p>
+            <p class="subtituloTypo">$${producto.precio}</p>
+            <p class="subtituloTypo">${producto.cantidad * producto.precio}</p>
+    
+    `;
+
+    contenedorCarrito.append(div);
+
+   total += producto.cantidad * producto.precio;
+    
+});
+
+const contenedorSubtotal = document.querySelector("#sub-total");
+
+const agregarSubtotal = () => {
+    let div = document.createElement("div");
+    div.classList.add("flexSubtotal", "descuentoTypo");
+    div.innerHTML = `
+        <p> Subtotal </p>
+        <p>$ ${total} </p>
+    `;
+    
+    contenedorSubtotal.append(div);
+    console.log(total);
+}
+
+agregarSubtotal();
+
+const contenedorTotal = document.querySelector("#totalisimo");
+
+const agregarTotalisimo = (nuevototal) => {
+    contenedorTotal.innerHTML = "";
+    let div = document.createElement("div");
+    div.classList.add("flexSubtotal", "totalTypo");
+    div.innerHTML = `
+        <p> Total </p>
+        <p>$ ${nuevototal} </p>
+    `;
+    
+    contenedorTotal.append(div);
+    console.log(total);
+}
+
+agregarTotalisimo(total);
+
+// formulario descuento
+
+const alertaForm = document.querySelector("#alerta-form");
+const alertaImput = document.querySelector("#cod-descuento");
+const alertaSubmit = document.querySelector("#alerta-submit");
+
+alertaForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const codigoIngresado = alertaImput.value.trim();
+    let descuento = descuentos[codigoIngresado];
+
+    if (descuento) {
+        valordescuento = descuento;
+        let nuevototal= total - (total * descuento);
+        agregarTotalisimo(nuevototal);
+        mostrardescuento();
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Codigo Incorrecto",
+            text: "El codigo de descuento ingresado no es correcto!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
+
+    }
+
+});
+
+const contenedorDescuento = document.querySelector("#codigo-desc");
+
+const mostrardescuento = () => {
+    contenedorDescuento.innerHTML="";
+    let div = document.createElement("div");
+    div.classList.add("flexSubtotal", "descuentoTypo");
+    div.innerHTML = `
+        <p> Descuento </p>
+        <p> ${valordescuento * 100} %</p>
+    `;
+    
+    contenedorDescuento.append(div);
+};
---
+++ js/main.js
@@ -0,0 +1,188 @@
+const productosDestacados = [
+    {
+        id: "chaleco-otto",
+        titulo: "Chaleco Otto",
+        precio: 3000,
+        img: "./assets/chaleco_otto.jpg",
+        imgcar: "../assets/chaleco_otto.jpg",
+    },
+    {
+        id: "camisa-cuadros",
+        titulo: "Camisa Cuadros",
+        precio: 5000,
+        img: "./assets/camisa_cuadros.jpg",
+        imgcar: "../assets/camisa_cuadros.jpg",
+    },
+    {
+        id: "remera-tsuki",
+        titulo: "Remera Tsuki",
+        precio: 3500,
+        img: "./assets/remera_tsuki.jpg",
+        imgcar: "../assets/remera_tsuki.jpg",
+    },
+    {
+        id: "vestido-lilo",
+        titulo: "Vestido Lilo",
+        precio: 12000,
+        img: "./assets/vestido_lilo.jpg", 
+        imgcar: "../assets/vestido_lilo.jpg",    
+    },
+    {
+        id: "camisa-yoga",
+        titulo: "Camisa Yoga",
+        precio: 8000,
+        img: "./assets/camisa_yoga.jpg",
+        imgcar: "../assets/camisa_yoga.jpg",   
+    },
+    {
+        id: "capri-geminis",
+        titulo: "Capri Geminis",
+        precio: 12000,
+        img: "./assets/capri_geminis.jpg", 
+        imgcar: "../assets/capri_geminis.jpg",   
+    }
+
+];
+
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+const contenedorProductosDestacados = document.querySelector("#productos");
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#carrito-productos");
+const carritoTotal = document.querySelector("#carrito-total");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const irAlCarrito = document.querySelector("#ir-al-carrito");
+
+
+productosDestacados.forEach((producto) => {
+
+    let div = document.createElement("div");
+ 
+    div.innerHTML = `
+        <img src="${producto.img}">
+        <div class="nameFlex">
+        <p class="titleCard">${producto.titulo}</p>
+        <ion-icon name="heart-outline" class="heartIcon"></ion-icon>
+        </div>
+        <p class="priceTypo marginPrice">$ ${producto.precio}</p>    
+    
+    `;
+
+    let button = document.createElement("button");
+    button.classList.add("cardButton");
+    button.innerText = "Agregar al carrito";
+    button.addEventListener("click", () => {
+        agregarAlCarrito(producto);
+    })
+
+    div.append(button);
+    contenedorProductosDestacados.append(div);
+});
+
+const agregarAlCarrito = (producto) => {
+    let carritoConProductos = carrito.find((articulo) => articulo.id === producto.id);
+
+    if (carritoConProductos) {
+        carritoConProductos.cantidad++;
+    } else {
+        carrito.push({...producto, cantidad:1});
+    }
+
+    actualizarCarrito();
+
+    Toastify({
+        text: "Agregaste " + producto.titulo + " al carrito",
+        avatar: producto.img,
+        duration: 2500,
+        close: true,
+        className: "toastFormato",
+        className: "toastTypo",
+        style: {
+          background: "#B47F7D",
+          color: "#ffff"
+        },
+      }).showToast();
+    
+}
+
+function actualizarCarrito() {
+    if (carrito.length === 0) {
+        carritoVacio.classList.remove("emptyclass");
+        carritoProductos.classList.add("emptyclass");
+        vaciarCarrito.classList.add("emptyclass");
+    } else {
+        carritoVacio.classList.add("emptyclass");
+        carritoProductos.classList.remove("emptyclass");
+        vaciarCarrito.classList.remove("emptyclass");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach((producto) => {
+            let div = document.createElement("div");
+            div.classList.add("productosFormato", "subtituloTypo");
+            div.innerHTML = `
+                <p>${producto.titulo}</p>
+                <p>${producto.cantidad}</p>
+                <p>$${producto.precio}</p>
+                <p>$${producto.cantidad * producto.precio}</p>
+            
+            `;
+            let button = document.createElement("button");
+            button.classList.add("btnEliminar");
+            button.innerHTML = `
+                <ion-icon name="trash-outline"></ion-icon>
+            `;
+            button.addEventListener("click", () => {
+                eliminarDelCarrito(producto);
+            })
+
+            div.append(button);
+            carritoProductos.append(div);
+        })
+    }
+    actualizarTotal();
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+actualizarCarrito();
+
+function eliminarDelCarrito(producto) {
+    const indice = carrito.findIndex((articulo) => articulo.id === producto.id);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+function actualizarTotal() {
+    let sumaTotal = carrito.reduce((acc, product) => acc + (product.precio * product.cantidad), 0 );
+    carritoTotal.innerText = "$" + sumaTotal;
+}
+
+vaciarCarrito.addEventListener("click", () => {
+    const cantidadTotal = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
+    Swal.fire({
+        title: "¿Estas seguro de dejar sin productos el carrito?",
+        text: "No podras revertir este proceso",
+        icon: "warning",        
+        showCancelButton: true,
+        confirmButtonColor: "#4A8FA5",
+        cancelButtonColor: "#B47F7D",
+        confirmButtonText: "Si, borrar todo!",
+        customClass: {
+            popup: "sweetFormat",
+            text: "sweetTypo"
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+            carrito.length = 0;
+            actualizarCarrito();
+          Swal.fire({
+            title: "Carrito Vacio!",
+            text: "El carrito quedo vacio",
+            icon: "success",
+            customClass: {
+                popup: 'sweetFormat',
+                text: "sweetTypo"
+              }
+          });
+        }
+      });
+})
