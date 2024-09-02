5d3962bae23eb3a7d90de614df5c44920cf6daee
eugebaspineiro <euge.baspineiro@gmail.com>
2024-09-02 17:37:41 -0300

    agregando la funcion de eliminar producto y vaciar carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,8 @@
 const contenedorCarrito = document.querySelector("#pag-carrito");
+const carritoVacio = document.querySelector("#carrito-vacio");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
+
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 let total= 0;
 
@@ -11,34 +15,114 @@ const descuentos = {
     "DESC20" : 0.20,
 };
 
-let carritoLS = JSON.parse(localStorage.getItem("carrito"));
+// carrito
 
-carritoLS.forEach(producto => {
-    let div = document.createElement("div");
-    div.classList.add("gridProd");
-    div.innerHTML= `
-            <div class="prodNuevoFlex">
-            <img src= "${producto.imgcar}" class="productoTamano">                  
-            <p class="subtituloTypo">${producto.titulo}</p>
-            </div>   
+function actualizarCarrito() {
+    if (carrito.length === 0){
+        carritoVacio.classList.remove("emptyclass");
+        contenedorCarrito.classList.add("emptyclass");
+        vaciarCarrito.classList.remove("emptyclass");
+        contenedorSubtotal.classList.add("emptyclass"); 
+        contenedorTotal.classList.add("emptyclass");   
+        contenedorDescuento.classList.add("emptyclass");    
+    } else {
+        carritoVacio.classList.add("emptyclass");
+        contenedorCarrito.classList.remove("emptyclass");
+        vaciarCarrito.classList.remove("emptyclass");
+
+        contenedorCarrito.innerHTML = "";
+        total = 0;
+
+        contenedorCarrito.innerHTML = "";
+        carrito.forEach((producto) => {
+            let div = document.createElement("div");
+            div.classList.add("gridProd");
+            div.innerHTML = `
+                 <div class="prodNuevoFlex">
+                <img src= "${producto.imgcar}" class="productoTamano">                  
+                <p class="subtituloTypo">${producto.titulo}</p>
+                </div>   
                                 
-            <p class="subtituloTypo">${producto.cantidad}</p>
-            <p class="subtituloTypo">$${producto.precio}</p>
-            <p class="subtituloTypo">${producto.cantidad * producto.precio}</p>
-            <ion-icon name="trash-outline" class= "ionTamano"></ion-icon>
-         
-    
-    `;
+                <p class="subtituloTypo">${producto.cantidad}</p>
+                <p class="subtituloTypo">$${producto.precio}</p>
+                <p class="subtituloTypo">${producto.cantidad * producto.precio}</p>
 
-    contenedorCarrito.append(div);
+            `;
 
-   total += producto.cantidad * producto.precio;
-    
-});
+            let button = document.createElement("button");
+            button.classList.add("btnEliminar");
+            button.innerHTML = `
+                <ion-icon name="trash-outline"></ion-icon>
+            `;
+            button.addEventListener("click", () => {
+                eliminarDelCarrito(producto);
+            })
+
+            div.append(button);
+            contenedorCarrito.append(div);
+
+            total += producto.cantidad * producto.precio;
+        });
+
+        agregarSubtotal();
+        agregarTotalisimo(total);
+    }
+}
+
+
+function eliminarDelCarrito (producto) {
+    const indice = carrito.findIndex((articulo) => articulo.id === producto.id);
+    carrito.splice(indice, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    actualizarCarrito();
+}
+
+
+
+vaciarCarrito.addEventListener("click", () => {
+    const cantidadTotal = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
+    Swal.fire({
+        title: "¿Estas seguro de dejar sin productos el carrito?",
+        text: "No podras revertir este proceso",
+        icon: "warning",        
+        showCancelButton: true,
+        confirmButtonColor: "#4A8FA5",
+        cancelButtonColor: "#B47F7D",
+        confirmButtonText: "Si, borrar todo!",
+        customClass: {
+            popup: "sweetFormat",
+            text: "sweetTypo"
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+            carrito.length = 0;
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            actualizarCarrito();
+          Swal.fire({
+            title: "Carrito Vacio!",
+            text: "El carrito quedo vacio",
+            icon: "success",
+            customClass: {
+                popup: 'sweetFormat',
+                text: "sweetTypo"
+              }
+          });
+        }
+      });
+})
+
+
+// descuento total
 
 const contenedorSubtotal = document.querySelector("#sub-total");
 
 const agregarSubtotal = () => {
+
+    if (carrito.length === 0){
+        contenedorDescuento.classList.add("emptyclass");
+    }
+    
+    contenedorSubtotal.innerHTML = "";
     let div = document.createElement("div");
     div.classList.add("flexSubtotal", "descuentoTypo");
     div.innerHTML = `
@@ -47,10 +131,9 @@ const agregarSubtotal = () => {
     `;
     
     contenedorSubtotal.append(div);
-    console.log(total);
 }
 
-agregarSubtotal();
+// agregarSubtotal();
 
 const contenedorTotal = document.querySelector("#totalisimo");
 
@@ -66,8 +149,6 @@ const agregarTotalisimo = (nuevototal) => {
     contenedorTotal.append(div);
     console.log(total);
 }
-
-agregarTotalisimo(total);
 
 // formulario descuento
 
@@ -111,3 +192,5 @@ const mostrardescuento = () => {
     
     contenedorDescuento.append(div);
 };
+
+actualizarCarrito();
